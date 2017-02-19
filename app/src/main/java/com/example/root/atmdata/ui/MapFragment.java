b4b20ba4b374f1857b8df48fb2aa9d504ad00641
Mandira <bhattarai.mandira155@gmail.com>
2017-02-19 16:55:37 +0545

    getting current location

--- app/src/main/java/com/example/root/atmdata/ui/MapFragment.java
+++ app/src/main/java/com/example/root/atmdata/ui/MapFragment.java
@@ -1,9 +1,21 @@
 package com.example.root.atmdata.ui;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
 import com.example.root.atmdata.AtmDetails;
@@ -11,6 +23,8 @@ import com.example.root.atmdata.R;
 import com.example.root.atmdata.base.BaseFragment;
 import com.example.root.atmdata.model.Atm;
 import com.example.root.atmdata.model.Bank;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -24,6 +38,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import static com.google.android.gms.internal.zzs.TAG;
+
 /**
  * Plot {@link Atm} into map
  */
@@ -33,6 +49,11 @@ public class MapFragment extends BaseFragment implements OnMapReadyCallback, Goo
     private List<Marker> markerList;
     private GoogleMap googleMap;
     private HashMap<String, Bank> bankMap;
+    private GoogleApiClient client;
+    LocationManager lm;
+    LocationListener ll;
+    Location l;
+    LatLng pos;
 
     public static MapFragment newInstance(List<Bank> bankList){
         MapFragment mapFragment=new MapFragment();
@@ -49,6 +70,7 @@ public class MapFragment extends BaseFragment implements OnMapReadyCallback, Goo
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         // initialize map
         SupportMapFragment fragment=new SupportMapFragment();
         getChildFragmentManager().beginTransaction().add(R.id.map_container,fragment).commit();
@@ -58,10 +80,39 @@ public class MapFragment extends BaseFragment implements OnMapReadyCallback, Goo
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
-        plotAtmList(bankList);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            lm.requestLocationUpdates(lm.NETWORK_PROVIDER, 0, 0, ll);
+        googleMap.setMyLocationEnabled(true);
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+
+            return;
+        }
+        Criteria criteria = new Criteria();
+
+        // Getting the name of the best provider
+        String provider = lm.getBestProvider(criteria, true);
+
+        // Getting Current Location
+        Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        if (location != null) {
+            // Getting latitude of the current location
+            double latitude = location.getLatitude();
+
+            // Getting longitude of the current location
+            double longitude = location.getLongitude();
+
+            // Creating a LatLng object for the current location
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            googleMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+            plotAtmList(bankList);
+        }
     }
 
-    private static final String TAG = "MapFragment";
+   // private static final String TAG = "MapFragment";
 
     void plotAtmList(List<Bank> bankList){
         Log.d(TAG, "plotAtmList() called with: bankList = [" + bankList + "]");
